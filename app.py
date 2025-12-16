45246db3aecc516b6960f4fba6f43b89a6e6d46e
SOOHWAN <sulgugu122333@gmail.com.com>
2025-12-16 15:14:18 +0900

    모델이름 수정

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ safety_system_prompt = """
 
 # 모델 초기화 (위에서 만든 선생님 규칙을 적용)
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
+    model_name="models/gemini-1.5-flash-latest",
     system_instruction=safety_system_prompt
 )
 
